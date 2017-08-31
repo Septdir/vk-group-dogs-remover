90588cd905df2f0d6e4bf9a6ab24052267c752d4
Septdir <sep260889>
2017-08-31 19:19:04 +0300

    First Version

---
+++ assets/ajax.js
@@ -0,0 +1,130 @@
+/*
+ *  * @package    VK - Group dogs remover
+ *  * @version    1.0.0
+ *  * @author     Igor Berdicheskiy - septdir.ru
+ *  * @copyright  Copyright (c) 2013 - 2017 Igor Berdicheskiy. All rights reserved.
+ *  * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ *  * @link       https://septdir.ru
+ */
+
+$(document).ready(function () {
+	$('#form').on('submit', function () {
+		$('#result').html('');
+		$('#form').find('input[name="offset"]').val(0);
+		$('#progressbar').attr('value', '0').attr('max', '10000');
+		$(counter).find('.dogs').html('0');
+		getMembers();
+		return false;
+	});
+	function removeDogs() {
+		var form = $('#form');
+		var button = $(form).find('button');
+		var progressbar =  $('#progressbar');
+		var counter = $('#counter');
+		var task = 'removeUser';
+		var dogs = $('#result').find('[data-dog]');
+		var dogsTotal = $(counter).find('.dogs').text() *1;
+		$(progressbar).attr('max', dogsTotal);
+
+		if (dogs.length > 0 ) {
+			var dog = dogs[0];
+			var id = $(dog).data('dog');
+			var params = $(form).serializeArray();
+			params.push({'name': 'user_id', 'value': id });
+
+			$.ajax({
+				type: 'POST',
+				dataType: 'json',
+				url: 'assets/api.php',
+				data: {
+					'params': params,
+					'task': task
+				},
+				beforeSend: function (response) {
+					$(button).attr('disabled', 'disabled');
+					$(counter).removeClass('uk-hidden');
+					$(progressbar).removeClass('uk-hidden');
+					$('#porgresslabel').find('.remove').removeClass('uk-hidden');
+				},
+				success: function (response) {
+					var current = $(counter).find('.remove').text() * 1;
+					var total = current + 1;
+					$(dog).remove();
+					$(counter).find('.remove').text(total);
+					$('#progressbar').attr('value', total);
+					setTimeout(removeDogs, 1500);
+
+				}
+			});
+
+		}
+		else {
+			$(button).removeAttr('disabled');
+			$(progressbar).addClass('uk-hidden');
+			$('#progressbar').attr('value', '0').attr('max', '10000');
+			$('#porgresslabel').find('.remove').addClass('uk-hidden');
+		}
+
+	}
+
+	function getMembers() {
+		var form = $('#form');
+		var params = $(form).serializeArray();
+		var button = $(form).find('button');
+		var progressbar =  $('#progressbar');
+		var counter = $('#counter');
+		var task = 'getMembers';
+		$.ajax({
+			type: 'POST',
+			dataType: 'json',
+			url: 'assets/api.php',
+			data: {
+				'params': params,
+				'task': task
+			},
+			beforeSend: function (response) {
+				$(button).attr('disabled', 'disabled');
+				$(counter).removeClass('uk-hidden');
+				$(counter).find('.remove').html('0');
+				$(progressbar).removeClass('uk-hidden');
+				$('#porgresslabel').find('.members').removeClass('uk-hidden');
+			},
+			success: function (response) {
+				var count = response.count * 1;
+				var offsetfiled = $(form).find('input[name="offset"]');
+				var offset = $(offsetfiled).val() * 1;
+				var newoffset = count + offset;
+				var step = $(form).find('input[name="count"]').val() * 1;
+				var dogs = $('#counter').find('.dogs').text() *1;
+
+				$(offsetfiled).val(newoffset);
+				$(counter).find('.total').html(newoffset + '/' + response.total);
+				$(counter).find('.dogs').html(dogs + (response.dogs *1));
+
+
+				var progressMax = $(progressbar).attr('max');
+				if ( progressMax != response.total) {
+					$(progressbar).attr('max', response.total);
+				}
+				$(progressbar).attr('value', newoffset);
+
+
+				$(response.html).appendTo($('#result'));
+
+				if (count == step) {
+					setTimeout(getMembers, 1500);
+				}
+				else {
+					$(button).removeAttr('disabled');
+					$(progressbar).addClass('uk-hidden');
+					$('#progressbar').attr('value', '0').attr('max', '10000');
+					$('#porgresslabel').find('.members').addClass('uk-hidden');
+					removeDogs();
+				}
+
+
+			}
+		});
+	}
+	
+});
---
+++ assets/api.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ *  * @package    VK - Group dogs remover
+ *  * @version    1.0.0
+ *  * @author     Igor Berdicheskiy - septdir.ru
+ *  * @copyright  Copyright (c) 2013 - 2017 Igor Berdicheskiy. All rights reserved.
+ *  * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ *  * @link       https://septdir.ru
+ */
+
+ini_set('display_errors', 1);
+ini_set('error_reporting', 2047);
+ini_set('max_execution_time', 0);
+
+class vkApi
+{
+	protected function send($method = '', $params = array())
+	{
+		$response                    = new stdClass();
+		$response->error             = 'error';
+		$response->error_description = 'curl_error';
+
+		if (!empty($method) && count($params) > 0)
+		{
+			$query = array();
+			foreach ($params as $param)
+			{
+				$query[$param['name']] = $param['value'];
+			}
+			$url = 'https://api.vk.com/method/' . $method . '?' . http_build_query($query);
+
+			if (function_exists('curl_init'))
+			{
+				unset($response->error);
+				unset($response->error_description);
+				$param = parse_url($url);
+				if ($curl = curl_init())
+				{
+					curl_setopt($curl, CURLOPT_URL, $param['scheme'] . '://' . $param['host'] . $param['path']);
+					curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+					curl_setopt($curl, CURLOPT_POST, true);
+					curl_setopt($curl, CURLOPT_POSTFIELDS, $param['query']);
+					curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+					$response = curl_exec($curl);
+					curl_close($curl);
+					$response = json_decode($response);
+
+				}
+			}
+		}
+
+		return $response;
+	}
+
+	public function getMembers($params)
+	{
+		$method = 'groups.getMembers';
+		$api    = $this->send($method, $params);
+
+		$users        = new stdClass();
+		$users->total = $api->response->count;
+		$users->count = count($api->response->users);
+		$users->array = $api->response->users;
+
+		return $users;
+	}
+
+	public function getDogs($users)
+	{
+		$dogs = array();
+		foreach ($users as $user)
+		{
+			if (!empty($user->deactivated))
+			{
+				$dogs[$user->uid] = $user;
+			}
+
+		}
+
+		return $dogs;
+	}
+
+	public function getMembersHTML($users)
+	{
+		$html = '';
+		foreach ($users as $user)
+		{
+			$dogsdata = '';
+			if (!empty($user->deactivated))
+			{
+				$dogsdata = ' data-dog="' . $user->uid . '"';
+			}
+			$name = $user->first_name . ' ' . $user->last_name;
+			$html .= '<div id="' . $user->uid . '" class="uk-width-1-6@m"' . $dogsdata . ' >';
+			$html .= '<div class="uk-card uk-card-default uk-text-center uk-height-1-1">';
+			$html .= '<div class="">';
+			$html .= '<img src="' . $user->photo_max . '" alt="' . $name . '" class="uk-width-1-1" />';
+			$html .= '</div>';
+			$html .= '<h3 class="uk-h5 uk-margin-remove uk-text-truncate uk-padding-small">' . $name . '</h3>';
+			$html .= '</div>';
+			$html .= '</div>';
+		}
+
+		return $html;
+	}
+
+	public function removeUser($params)
+	{
+		$method = 'groups.removeUser';
+		$this->send($method, $params);
+
+		return true;
+	}
+}
+
+
+$data   = $_POST;
+$params = $data['params'];
+$task   = $data['task'];
+$api    = new vkApi();
+if ($task == 'getMembers')
+{
+	$users = $api->getMembers($params);
+	$dogs  = $api->getDogs($users->array);
+	$html  = $api->getMembersHTML($users->array);
+
+	$response        = new stdClass();
+	$response->total = $users->total;
+	$response->count = $users->count;
+	$response->dogs  = count($dogs);
+	$response->html  = $html;
+	$response        = json_encode($response);
+	echo $response;
+}
+if ($task == 'removeUser')
+{
+	$response = $api->removeUser($params);
+	echo json_encode($response);
+
+}
